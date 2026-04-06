f5c22faf462b4085fc82e0c72b6bf95f2f8ab3dc
Stonerainbow19 <wesleygoncalvesribeiri@gmail.com>
2026-04-05 23:20:55 -0300

    start code

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,111 @@
 #include <Arduino.h>
+#include <Dabble.h>
+
+#define CUSTOM_SETTINGS
+#define INCLUDE_GAMEPAD_MODULE
+
 
 int IN1, IN2, IN3, IN4;
 int led;
 
+void initializePins() {
+  IN1 = 2;
+  IN2 = 3;
+  IN3 = 4;
+  IN4 = 5;
+  
+  led = 13;
+}
+
+
+void controlBluetooth(){
+  Dabble.processInput();             //this function is used to refresh data obtained from smartphone.Hence calling this function is mandatory in order to get data properly from your mobile.
+  Serial.print("KeyPressed: ");
+  if (GamePad.isUpPressed())
+  {
+    Serial.print("Up");
+  }
+
+  if (GamePad.isDownPressed())
+  {
+    Serial.print("Down");
+  }
+
+  if (GamePad.isLeftPressed())
+  {
+    Serial.print("Left");
+  }
+
+  if (GamePad.isRightPressed())
+  {
+    Serial.print("Right");
+  }
+
+  if (GamePad.isSquarePressed())
+  {
+    Serial.print("Square");
+  }
+
+  if (GamePad.isCirclePressed())
+  {
+    Serial.print("Circle");
+  }
+
+  if (GamePad.isCrossPressed())
+  {
+    Serial.print("Cross");
+  }
+
+  if (GamePad.isTrianglePressed())
+  {
+    Serial.print("Triangle");
+  }
+
+  if (GamePad.isStartPressed())
+  {
+    Serial.print("Start");
+  }
+
+  if (GamePad.isSelectPressed())
+  {
+    Serial.print("Select");
+  }
+  Serial.print('\t');
+
+  int a = GamePad.getAngle();
+  Serial.print("Angle: ");
+  Serial.print(a);
+  Serial.print('\t');
+  int b = GamePad.getRadius();
+  Serial.print("Radius: ");
+  Serial.print(b);
+  Serial.print('\t');
+  float c = GamePad.getXaxisData();
+  Serial.print("x_axis: ");
+  Serial.print(c);
+  Serial.print('\t');
+  float d = GamePad.getYaxisData();
+  Serial.print("y_axis: ");
+  Serial.println(d);
+  Serial.println();
+}
 
 void setup() {
+  initializePins();
   pinMode(led, OUTPUT);
-  
   pinMode(IN1, OUTPUT);
   pinMode(IN2, OUTPUT);
   pinMode(IN3, OUTPUT);
   pinMode(IN4, OUTPUT);
+  Serial.begin(19200);
+  Dabble.begin(9600);
 }
 
 void loop() {
-  digitalWrite(IN1,HIGH);
-  digitalWrite(IN2,LOW); 
+  controlBluetooth();
 }
+
+
 
 
 
