86f9c044745d30376a9e03882ef30910c5d33c65
Stonerainbow19 <wesleygoncalvesribeiri@gmail.com>
2026-04-20 22:53:03 -0300

    atualizacao

--- src/main.cpp
+++ src/main.cpp
@@ -1,108 +1,181 @@
 #include <Arduino.h>
-#include <Dabble.h>
+
 
 #define CUSTOM_SETTINGS
 #define INCLUDE_GAMEPAD_MODULE
+#include <Dabble.h>
 
+// Definicao de pinos
 
-int IN1, IN2, IN3, IN4;
+const int IN1 = 4, IN2 = 5, IN3 = 6, IN4 = 7, WEAPON1 = 10, WEAPON2 = 8, ENC = 9, ENA = A2, ENB = A1, led = 13;
+int spdRightMotor, spdLeftMotor, spdWeapon;
+
+bool offWeapon;
 int led;
 
-void initializePins() {
-  IN1 = 2;
-  IN2 = 3;
-  IN3 = 4;
-  IN4 = 5;
-  
-  led = 13;
+
+void moveRobot(int spd1, int spd2){
+  // Move Forward
+  if (spd1 > 0 && spd2 > 0){
+    digitalWrite(IN1, HIGH);
+    digitalWrite(IN2, LOW);
+    digitalWrite(IN3, HIGH);
+    digitalWrite(IN4, LOW);
+    analogWrite(ENA, spd1);
+    analogWrite(ENB, spd2);
+  }
+  // Move Backward
+  else if (spd1 < 0 && spd2 < 0){
+    digitalWrite(IN1, LOW);
+    digitalWrite(IN2, HIGH);
+    digitalWrite(IN3, LOW);
+    digitalWrite(IN4, HIGH);
+    analogWrite(ENA, -spd1);
+    analogWrite(ENB, -spd2);
+  }
+  // Turn Right
+  else if (spd1 > 0 && spd2 < 0){
+    digitalWrite(IN1, HIGH);
+    digitalWrite(IN2, LOW);
+    digitalWrite(IN3, LOW);
+    digitalWrite(IN4, HIGH);
+    analogWrite(ENA, spd1);
+    analogWrite(ENB, -spd2);
+  }
+  // Turn Left
+  else if (spd1 < 0 && spd2 > 0){
+    digitalWrite(IN1, LOW);
+    digitalWrite(IN2, HIGH);
+    digitalWrite(IN3, HIGH);
+    digitalWrite(IN4, LOW);
+    analogWrite(ENA, -spd1);
+    analogWrite(ENB, spd2);
+  }
+  // Stop
+  else {
+    digitalWrite(IN1, LOW);
+    digitalWrite(IN2, LOW);
+    digitalWrite(IN3, LOW);
+    digitalWrite(IN4, LOW);
+    analogWrite(ENA, 0);
+    analogWrite(ENB, 0);
+  }
+
 }
 
+void moveRobotJoystick(float x_axis, float y_axis){
+  // Ajeitando o valor do x e y
+  x_axis = x_axis / 7;
+  y_axis = y_axis / 7;
 
-void controlBluetooth(){
-  Dabble.processInput();             //this function is used to refresh data obtained from smartphone.Hence calling this function is mandatory in order to get data properly from your mobile.
-  Serial.print("KeyPressed: ");
-  if (GamePad.isUpPressed())
-  {
-    Serial.print("Up");
+  int spd1 = (int)(y_axis * 255);
+  int spd2 = (int)(y_axis * 255);
+
+  if (x_axis > 0) { 
+    spd1 -= (int)(x_axis * 255);
+    spd2 += (int)(x_axis * 255);
+  } else if (x_axis < 0) { 
+    spd1 += (int)(-x_axis * 255);
+    spd2 -= (int)(-x_axis * 255);
   }
 
-  if (GamePad.isDownPressed())
-  {
-    Serial.print("Down");
-  }
+  spd1 = constrain(spd1, -255, 255);
+  spd2 = constrain(spd2, -255, 255);
 
-  if (GamePad.isLeftPressed())
-  {
-    Serial.print("Left");
-  }
-
-  if (GamePad.isRightPressed())
-  {
-    Serial.print("Right");
-  }
-
-  if (GamePad.isSquarePressed())
-  {
-    Serial.print("Square");
-  }
-
-  if (GamePad.isCirclePressed())
-  {
-    Serial.print("Circle");
-  }
-
-  if (GamePad.isCrossPressed())
-  {
-    Serial.print("Cross");
-  }
-
-  if (GamePad.isTrianglePressed())
-  {
-    Serial.print("Triangle");
-  }
-
-  if (GamePad.isStartPressed())
-  {
-    Serial.print("Start");
-  }
-
-  if (GamePad.isSelectPressed())
-  {
-    Serial.print("Select");
-  }
-  Serial.print('\t');
-
-  int a = GamePad.getAngle();
-  Serial.print("Angle: ");
-  Serial.print(a);
-  Serial.print('\t');
-  int b = GamePad.getRadius();
-  Serial.print("Radius: ");
-  Serial.print(b);
-  Serial.print('\t');
-  float c = GamePad.getXaxisData();
-  Serial.print("x_axis: ");
-  Serial.print(c);
-  Serial.print('\t');
-  float d = GamePad.getYaxisData();
-  Serial.print("y_axis: ");
-  Serial.println(d);
-  Serial.println();
+  moveRobot(spd1, spd2);
 }
 
 void setup() {
-  initializePins();
+  
   pinMode(led, OUTPUT);
+  
   pinMode(IN1, OUTPUT);
   pinMode(IN2, OUTPUT);
   pinMode(IN3, OUTPUT);
   pinMode(IN4, OUTPUT);
+
+  pinMode(WEAPON1, OUTPUT);
+  pinMode(WEAPON2, OUTPUT);
+  
+  // Variaveis da arma; 
+  offWeapon = true;
+  spdWeapon = 255;
+
   Serial.begin(19200);
   Dabble.begin(9600);
 }
 
 void loop() {
-  controlBluetooth();
+   Dabble.processInput(); 
+  
+  float x = GamePad.getXaxisData();
+  Serial.print("x_axis: ");
+  Serial.print(x);
+  Serial.print('\t');
+  float y = GamePad.getYaxisData();
+  Serial.print("y_axis: ");
+  Serial.println(y);
+  Serial.println();
+
+
+  // Modo - Joystick
+  if(x != 0 & y != 0) {
+      moveRobotJoystick(x,y);
+  }
+  // Modo - Dpad
+  else{
+    spdRightMotor = 255;
+    spdLeftMotor = 255;
+    if (GamePad.isUpPressed())
+    {
+      Serial.println("Up");
+    }
+    else if (GamePad.isDownPressed())
+    {
+      Serial.print("Down");
+      spdRightMotor = -spdRightMotor;
+      spdLeftMotor = -spdLeftMotor;
+    }
+    else if (GamePad.isLeftPressed())
+    {
+      Serial.print("Left");
+      spdLeftMotor = -spdLeftMotor;
+    }
+
+    else if (GamePad.isRightPressed())
+    {
+      Serial.print("Right");
+      spdRightMotor = -spdRightMotor;
+    } else {
+       spdRightMotor = 0;
+       spdLeftMotor = 0;
+    }
+    moveRobot(spdLeftMotor,spdRightMotor);
+
+  }
+
+
+  
+  // Controle da arma
+  unsigned long lastPress = 0;
+  if (GamePad.isCirclePressed())
+  { 
+    if(millis() - lastPress > 300){
+      offWeapon = !offWeapon;
+      lastPress = millis();
+      Serial.print("Circle");
+    }
+  }
+
+  if(offWeapon){
+    digitalWrite(WEAPON1, HIGH);
+    digitalWrite(WEAPON2, LOW);
+    analogWrite(ENC, spdWeapon);
+  } else{
+    digitalWrite(WEAPON1, HIGH);
+    digitalWrite(WEAPON2, HIGH);
+    analogWrite(ENC, 0); 
+  }
 }
 
 
